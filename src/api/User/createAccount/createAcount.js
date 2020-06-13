13473b776b5f097c6ab33453a8261e96200c39aa
Korea-code <rlawlgy93@gmail.com>
2020-06-13 19:02:50 +0900

    Update createAccount error message

--- src/api/User/createAccount/createAcount.js
+++ src/api/User/createAccount/createAcount.js
@@ -4,6 +4,15 @@ export default {
   Mutation: {
     createAcount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const existsUser = await prisma.$exists.user({ username });
+      const existsEmail = await prisma.$exists.user({ email });
+      if (existsEmail) {
+        throw Error("This email is taken");
+      }
+      if (existsUser) {
+        throw Error("This username is taken");
+      }
+
       await prisma.createUser({
         username,
         email,
